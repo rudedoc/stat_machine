89d217a8de99a4e74a9eb36bbd0381b7a5247220
Mark Finlay <finlay.mark@gmail.com>
2025-12-27 20:54:41 +0000

    wip

--- app/models/price.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class Price < ApplicationRecord
-  belongs_to :competitor
-
-  validates :percentage, :captured_at, presence: true
-end
--- db/migrate/20251227175456_add_predictions_to_events.rb
+++
@@ -1,5 +0,0 @@
-class AddPredictionsToEvents < ActiveRecord::Migration[8.1]
-  def change
-    add_column :events, :predictions, :jsonb
-  end
-end
