ea55f1ab4d43ba49aa1444e755b427cdb51eb1fa
Mark Finlay <finlay.mark@gmail.com>
2025-12-30 16:41:35 +0000

    seeding FeedSources'

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,20 @@
+require "csv"
+
+feed_sources_path = Rails.root.join("db", "rss_feeds.csv")
+
+if File.exist?(feed_sources_path)
+  CSV.foreach(feed_sources_path, headers: true) do |row|
+    name = row["Source Name"].to_s.strip
+    feed_url = row["Feed URL"].to_s.strip
+    next if name.blank? || feed_url.blank?
+
+    FeedSource.find_or_initialize_by(feed_url: feed_url).tap do |feed|
+      feed.name = name
+      feed.save!
+    end
+  end
+end
+
 BetfairApi.import_all_data!
 
 FootballApiService.new.sync_matches
